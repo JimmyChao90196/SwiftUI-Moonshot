cbcdc274da1322c9576305fe620dc6ec6d2c152d
Jimmy <myname90196@gmail.com>
2024-03-28 15:25:24 +0800

    Initial Commit

---
+++ SwiftUI-Moonshot/SwiftUI_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MoonshotApp.swift
+//  SwiftUI-Moonshot
+//
+//  Created by JimmyChao on 2024/3/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
